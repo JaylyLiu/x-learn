681791483a54bded3e46977e98f3a98914845215
Jayly <jayly@italki.com>
2020-04-17 15:38:23 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2d604da5ffeaa472111b6d9b81684ec9.js"
+  "./precache-manifest.8461b015f61b8a687e0f39ba2f3196d6.js"
 );
 
 self.addEventListener('message', (event) => {
