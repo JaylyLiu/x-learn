f5eb68a02c3de5c1f81c5b3fb373fd8f7202a56e
Jayly <jayly@italki.com>
2020-04-17 12:03:39 +0800

    add detail footer

--- src/page/LessonDetail/LessonDetail.js
+++ src/page/LessonDetail/LessonDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import LessonDetailStyles from './LessonDetail.module.scss';
@@ -8,30 +9,39 @@ import LessonDetailStyles from './LessonDetail.module.scss';
 import hh from "./Imgs/hh.mp4";
 import ww from "./Imgs/ww.mp4";
 import uu from "./Imgs/uu.mp4";
-const styles = {...LessonDetailStyles};
+const styles = { ...LessonDetailStyles };
 
 function LessonDetail() {
-  
+  let history = useHistory();
+
   return (
     <div className={styles.lessonDetail}>
-      LessonDetail
-      
-<Carousel
- 
-  
-  addArrowClickHandler
->
-      <video controls autoPlay controlsList="nodownload" className={styles.video} >
-         <source src={hh} type="video/mp4" />
+      <Carousel
+        addArrowClickHandler
+      >
+        <video controls autoPlay controlsList="nodownload" className={styles.video} >
+          <source src={hh} type="video/mp4" />
         </video>
-      <video controls autoPlay controlsList="nodownload"  className={styles.video}>
-        <source src={ww} type="video/mp4" />
-      </video>
-      <video controls autoPlay controlsList="nodownload"  className={styles.video}>
-        <source src={uu} type="video/mp4" />
-      </video>
+        <video controls autoPlay controlsList="nodownload" className={styles.video}>
+          <source src={ww} type="video/mp4" />
+        </video>
+        <video controls autoPlay controlsList="nodownload" className={styles.video}>
+          <source src={uu} type="video/mp4" />
+        </video>
+      </Carousel>
 
-</Carousel>
+      <div className={styles.footer}>
+        <div className={styles.box}>
+          <div className={styles.left} onClick={() => history.push({ pathname: '/schedule' })}>
+            <span className={styles.top}>CNY 180</span>
+            <span className={styles.bottom}>GROUP</span>
+          </div>
+          <div className={styles.right} onClick={() => history.push({ pathname: '/schedule' })}>
+            <span className={styles.top}>CNY 200</span>
+            <span className={styles.bottom}>PRIVATE</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
